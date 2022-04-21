62a4e3fd7f5d173d0142e93e60f6d2761867d65d
Luan van der Westhuizen <luan.vdw@gmail.com>
2022-04-21 15:18:23 +0200

    Added prisma client and updated main function

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
+  // Connect the client
+  await prisma.$connect()
+
   const allUsers = await prisma.user.findMany()
   console.log(allUsers)
 }
